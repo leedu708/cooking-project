cb69f282169c6dac54dc8fad15266417d44999ab
Dustin <leedu708@gmail.com>
2016-04-05 21:43:15 -0400

    added toggle to recipe cards

--- app/assets/javascripts/controllers/HomeCtrl.js
+++ app/assets/javascripts/controllers/HomeCtrl.js
@@ -4,12 +4,27 @@ cooking.controller('HomeCtrl',
 
     $scope.init = function() {
       $scope.setRecipes();
+      $scope.flippedCards = [];
     };
 
     $scope.setRecipes = function() {
       recipeService.home().then(function(response) {
         $scope.recipes = response;
       });
+    };
+
+    $scope.flip = function(recipe) {
+      $scope.flippedCards.push(recipe);
+    };
+
+    $scope.closeFlip = function(removed) {
+      $scope.flippedCards = $scope.flippedCards.filter( function(recipe) {
+        return recipe.id !== removed.id;
+      });
+    };
+
+    $scope.flipped = function(recipe) {
+      return ($scope.flippedCards.indexOf(recipe) > -1);
     };
 
     $scope.init();
