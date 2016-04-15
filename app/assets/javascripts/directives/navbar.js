9c5e9633ecf59e1d3a2062cc7cbac819e9d075ff
leedu708 <leedu708@gmail.com>
2016-04-15 13:54:15 -0400

    navbar has dashboard logic

--- app/assets/javascripts/directives/navbar.js
+++ app/assets/javascripts/directives/navbar.js
@@ -3,13 +3,22 @@ cooking.directive('navbar', function() {
     restrict: 'E',
     templateUrl: '/templates/shared/navbar.html',
     scope: [],
-    controller: ['$scope', '$window', function($scope, $window) {
+    controller: ['$scope','$window', function($scope, $window) {
       $scope.width = $window.innerWidth;
 
       angular.element($window).bind('resize', function() {
         $scope.width = $window.innerWidth;
         $scope.$digest();
-      })
+      });
+
+      $scope.$on('$stateChangeSuccess', function(event, to) {
+        if (to.name.match(/dashboard/)) {
+          $scope.onDash = true;
+        } else {
+          $scope.onDash = false;
+        };
+      });
+      
     }]
   }
 })
